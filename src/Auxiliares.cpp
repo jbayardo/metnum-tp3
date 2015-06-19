0c648a56655bfb0eaa3859efa8c789afeb7df30c
Manuel Carrasco <mgcarrasco2012@gmail.com>
2015-06-19 01:19:13 -0300

    Terminada funcion de reduccion, ahora concuerda con el diseño de nuestros algoritmos

--- src/Auxiliares.cpp
+++ src/Auxiliares.cpp
@@ -31,11 +31,38 @@ int norma1(int i, int j, int ki, int kj)
     return abs(i - ki) + abs(j - kj);
 }
 
-// Toma la imagen original y una copia de ella (mismo tamaño)
-// Modifica la de output y le pone 0 en los lugares que serian los pixeles aumentados para un k dado
-void reducir(const Matrix& input, Matrix& output, int k)
+// Vos le tiras la imagen de test como input y el te retorna la matriz reducida, que le tenes que pasar a nuestros algoritmos.
+// Ej: Agarro la imagen A de nuestro test, la paso a matriz.
+// reducir agarra A, la reduce dado un k correcto (generando A').
+// agarras un algoritmo nuestro de zoom y le decis que aumente A', luego comparas el resultado que intento zoomear A' contra A.
+// NO OLVIDARSE DE HACER FREEE
+Matrix* reducir(const Matrix& input, int k)
 {
-    ItPixelesNuevos it(output.rows(), output.columns(), k);
+    // calculo nuevas dimensiones
+
+    // nos basamos en la relacion
+    // fila de la aumentada = fila de la original * (k+1)
+    // calculo la posicion de la ultima fila, y le aumento en uno asi obtengo el size de la fila
+    int f = ((input.rows()-1) / (k+1) ) + 1;
+    int c = ((input.columns()-1) / (k+1)) + 1;
+    Matrix *small = new Matrix(f, c);
+
+    for (int i = 0; i < input.rows(); i = i + k+1)
+    {
+        for (int j = 0; j < input.rows(); j = j+k+1)
+        {
+            // traducimos de la zoomeada a la 'original'
+            int f_ = i / (k+1);
+            int c_ = j / (k+1);
+
+            (*small)(f_, c_) = input(i,j);
+        }
+    }
+
+    // Esto ya lo hacen nuestros algoritmos al recibir una matriz, la copian, la agrandan y hacen estas cosas con los ceros.
+    // En parte esta funcion se vuelve un poco redundante, pero no va a hacer las cosas mucho mas lentas.
+    // Preferible confiar en lo que ya anda, que ir metiendo mano inecesariamente.
+    /*ItPixelesNuevos it(output.rows(), output.columns(), k);
     while(!it.end())
     {
         int i = it.get().first;
@@ -43,5 +70,7 @@ void reducir(const Matrix& input, Matrix& output, int k)
 
         output(i,j) = 0;
         ++it;
-    }
+    }*/
+
+    return small;
 }
