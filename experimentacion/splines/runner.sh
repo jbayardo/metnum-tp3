1f912e7a305807410759302bcdc58c921e64d8a1
Chris <chriscuneo93@gmail.com>
2015-07-16 00:24:47 -0300

    Datos todos los test

--- experimentacion/splines/runner.sh
+++ experimentacion/splines/runner.sh
@@ -64,3 +64,40 @@
 ./../tp ../originales/paisaje_2.csv paisaje_2_test_7_8.csv 401 401 7 5 8
 ./../tp ../originales/paisaje_2.csv paisaje_2_test_9_8.csv 401 401 9 5 8
 
+
+./../tp ../originales/geometrica_1.csv geometrica_1_test_1_201.csv 401 401 1 5 201
+./../tp ../originales/geometrica_1.csv geometrica_1_test_3_101.csv 401 401 3 5 101
+./../tp ../originales/geometrica_1.csv geometrica_1_test_4_81.csv 401 401 4 5 81
+./../tp ../originales/geometrica_1.csv geometrica_1_test_7_51.csv 401 401 7 5 51
+./../tp ../originales/geometrica_1.csv geometrica_1_test_9_41.csv 401 401 9 5 41
+
+./../tp ../originales/geometrica_2.csv geometrica_2_test_1_201.csv 401 401 1 5 201
+./../tp ../originales/geometrica_2.csv geometrica_2_test_3_101.csv 401 401 3 5 101
+./../tp ../originales/geometrica_2.csv geometrica_2_test_4_81.csv 401 401 4 5 81
+./../tp ../originales/geometrica_2.csv geometrica_2_test_7_51.csv 401 401 7 5 51
+./../tp ../originales/geometrica_2.csv geometrica_2_test_9_41.csv 401 401 9 5 41
+
+./../tp ../originales/rostro_2.csv rostro_2_test_1_201.csv 401 401 1 5 201
+./../tp ../originales/rostro_2.csv rostro_2_test_3_101.csv 401 401 3 5 101
+./../tp ../originales/rostro_2.csv rostro_2_test_4_81.csv 401 401 4 5 81
+./../tp ../originales/rostro_2.csv rostro_2_test_7_51.csv 401 401 7 5 51
+./../tp ../originales/rostro_2.csv rostro_2_test_9_41.csv 401 401 9 5 41
+
+./../tp ../originales/rostro_1.csv rostro_1_test_1_201.csv 401 401 1 5 201
+./../tp ../originales/rostro_1.csv rostro_1_test_3_101.csv 401 401 3 5 101
+./../tp ../originales/rostro_1.csv rostro_1_test_4_81.csv 401 401 4 5 81
+./../tp ../originales/rostro_1.csv rostro_1_test_7_51.csv 401 401 7 5 51
+./../tp ../originales/rostro_1.csv rostro_1_test_9_41.csv 401 401 9 5 41
+
+./../tp ../originales/paisaje_1.csv paisaje_1_test_1_201.csv 401 401 1 5 201
+./../tp ../originales/paisaje_1.csv paisaje_1_test_3_101.csv 401 401 3 5 101
+./../tp ../originales/paisaje_1.csv paisaje_1_test_4_81.csv 401 401 4 5 81
+./../tp ../originales/paisaje_1.csv paisaje_1_test_7_51.csv 401 401 7 5 51
+./../tp ../originales/paisaje_1.csv paisaje_1_test_9_41.csv 401 401 9 5 41
+
+./../tp ../originales/paisaje_2.csv paisaje_2_test_1_201.csv 401 401 1 5 201
+./../tp ../originales/paisaje_2.csv paisaje_2_test_3_101.csv 401 401 3 5 101
+./../tp ../originales/paisaje_2.csv paisaje_2_test_4_81.csv 401 401 4 5 81
+./../tp ../originales/paisaje_2.csv paisaje_2_test_7_51.csv 401 401 7 5 51
+./../tp ../originales/paisaje_2.csv paisaje_2_test_9_41.csv 401 401 9 5 41
+
